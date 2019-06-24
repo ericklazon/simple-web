807a19b496f30291beb6b9f2036b9dfc7e037ec1
Erick <elazon@uni.pe>
2019-06-24 12:25:47 -0500

    Updates

---
+++ precache-manifest.1d4fe75937a29c1838797daf12f258cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1aac6933471cc93c322",
+    "url": "/simple-web/static/js/runtime~main.d1aac693.js"
+  },
+  {
+    "revision": "7670a4953c48bd29305f",
+    "url": "/simple-web/static/js/main.7670a495.chunk.js"
+  },
+  {
+    "revision": "e990ccfaedfecdf9af24",
+    "url": "/simple-web/static/js/1.e990ccfa.chunk.js"
+  },
+  {
+    "revision": "7670a4953c48bd29305f",
+    "url": "/simple-web/static/css/main.f6618994.chunk.css"
+  },
+  {
+    "revision": "800f06394d694d25f75d11bf156cc34b",
+    "url": "/simple-web/index.html"
+  }
+];
